f46d190be75ab71016ca17b50d80aef9a4a6838b
Luiz Gustavo Chimenes <gustavochimenes@hotmail.com>
2022-06-20 13:45:49 -0300

    a

--- pyt_cursoemvideo/aula18/ex86yt.py
+++ pyt_cursoemvideo/aula18/ex86yt.py
@@ -1,17 +1,12 @@
 # Digitar 7 valores e cadastrá-los em uma única lista que os separe em pares e ímpares em ordem crecente
-lista = []
+lista = [[], []]
 for c in range(7):
     num = int(input("Digite um valor inteiro: "))
     if num % 2 == 0:
-        pares = []
-        pares.append(num)
-    if num % 2 == 1:
-        impares = []
-        impares.append(num)
-    lista.append(pares)
-lista.append(pares)
-lista.append(impares)
-pares.sort()
-impares.sort()
-print(f"O números pares foram {lista[0]}")
-print(f"Os números ímpares foram {lista[1]}")
+        lista[0].append(num)
+    elif num % 2 == 1:
+        lista[1].append(num)
+print(20*"-=")
+print(f"Os números pares foram : {lista[0]}")
+print(f"Os números ímpares foram: {lista[1]}")
+    
