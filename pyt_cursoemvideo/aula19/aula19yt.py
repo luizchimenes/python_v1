f21804868228a25540c79b22794b5ede7d56f110
Luiz Gustavo Chimenes <gustavochimenes@hotmail.com>
2022-07-02 19:28:45 -0300

    .

--- pyt_cursoemvideo/aula19/aula19yt.py
+++ pyt_cursoemvideo/aula19/aula19yt.py
@@ -1,3 +1,5 @@
 # Dicionários
-pessoas = {"Nome": "Luiz Gustavo", "Idade": 20, "Sexo": "M"}
-print(pessoas.items())
+'''pessoas = {"Nome": "Luiz Gustavo", "Idade": 20, "Sexo": "M"}
+print(pessoas.keys())
+for k, v in pessoas.items():
+    print(f"{k} = {v}")'''
